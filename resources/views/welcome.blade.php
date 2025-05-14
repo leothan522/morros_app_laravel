1c49748f6024c65cfa4040d0e8b41267bb5bd806
Yonathan Castillo <leothan522@gmail.com>
2025-05-14 12:02:35 -0400

    Laravel Lang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,24 +28,33 @@
                             href="{{ url('/home') }}"
                             class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal"
                         >
-                            Dashboard
+                            {{ __('Home') }}
                         </a>
                     @else
+
                         <a
                             href="{{ route('login') }}"
                             class="inline-block px-5 py-1.5 text-[#1b1b18] border border-transparent hover:border-[#19140035] rounded-sm text-sm leading-normal"
                         >
-                            Log in
+                            {{ __('Log in') }}
                         </a>
 
                         @if (Route::has('register'))
                             <a
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal">
-                                Register
+                                {{ __('Register') }}
                             </a>
                         @endif
                     @endauth
+
+                        <a
+                            href="{{ route('login') }}"
+                            class="inline-block px-5 py-1.5 text-[#1b1b18] border border-transparent hover:border-[#19140035] rounded-sm text-sm leading-normal"
+                        >
+                            {{ __('Dashboard') }}
+                        </a>
+
                 </nav>
             @endif
         </header>
