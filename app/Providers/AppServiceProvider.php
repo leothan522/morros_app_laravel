b6771eace6aeb834ee20cd16e8efe69e39970272
Yonathan Castillo <leothan522@gmail.com>
2025-05-14 10:48:47 -0400

    Laravel Jetstream application

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Customizing the asset URL Livewire's
+        Livewire::setUpdateRoute(function ($handle) {
+            return Route::post('/'.env('APP_ASSET_LIVEWIRE', 'laravel/public').'/livewire/update', $handle)->name('assetlivewire.update');
+        });
+
+        Livewire::setScriptRoute(function ($handle) {
+            return Route::get('/'.env('APP_ASSET_LIVEWIRE', 'laravel/public').'/livewire/livewire.js', $handle);
+        });
     }
 }
