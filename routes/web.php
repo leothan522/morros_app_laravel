4771b0c69c8d0aa4a45da39514e33638fa8f3f14
Yonathan Castillo <leothan522@gmail.com>
2025-05-14 20:37:13 -0400

    Instalacion de Bootstrap 5 con Vite

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('web.index');
 
 Route::middleware([
     'auth:sanctum',
@@ -14,4 +14,8 @@ Route::middleware([
     Route::get('/home', function () {
         return view('home');
     })->name('home');
+});
+
+Route::get('/bootstrap', function () {
+    return view('bootstrap');
 });
